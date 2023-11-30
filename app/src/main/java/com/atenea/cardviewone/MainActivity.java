212f52bfc95dbcef42834e6144096c31aa513825
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-11-29 21:29:28 -0500

    add android application

--- app/src/main/java/com/atenea/cardviewone/MainActivity.java
+++ app/src/main/java/com/atenea/cardviewone/MainActivity.java
@@ -3,6 +3,10 @@ package com.atenea.cardviewone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción a realizar cuando se hace clic en el botón "btn_back"
+                Toast.makeText(MainActivity.this, "Llamar a la anterior tarjeta", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MaterialButton btnNext = findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción a realizar cuando se hace clic en el botón "btn_next"
+                Toast.makeText(MainActivity.this, "Llamar a la siguiente tarjeta",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
